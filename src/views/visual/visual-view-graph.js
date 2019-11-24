e858578b9ea26b4ae5ab0f4445e8a258e90defb8
Sebastian <basti@slayer>
2019-11-24 23:22:12 +0900

    Added some description to the compression function.

--- src/views/visual/visual-view-graph.js
+++ src/views/visual/visual-view-graph.js
@@ -40,6 +40,7 @@ export class VisualViewGraph extends LitElement {
   }
 
   smoothGraphValues(value) {
+    // this function is used to determine how much large collections are shrinked and smaller collections are enlarged, resulting in a compression effect that makes the rendering of smaller entities more readable. a value of ** 1 means nothing is changed. The smaller the value is, the stronger the graph is 'compressed'.
     return value ** 0.33;
   }
 
