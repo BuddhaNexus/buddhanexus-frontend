a16e9be5f893b30e992bf9c374c99d5bf0c69529
Sebastian <basti@slayer>
2020-02-26 14:42:31 +0900

    Small bugfix.

--- src/api/actions.js
+++ src/api/actions.js
@@ -104,8 +104,6 @@ export const getFileTextAndParallels = async ({
       limit_collection,
       queryParams
     );
-    console.log('LIMIT COLLECTION', limit_collection);
-    console.log('URL', url);
     const response = await fetch(url);
     const json = await response.json();
     if (!response.ok) {
