acfa42459eb36fc526413a3bdcf57976e380ae34
Hubert Dworczynski <hubert.dworczynski@gmail.com>
2020-03-28 17:39:28 +0100

    add href links to static pages

--- src/views/static/static-view.styles.js
+++ src/views/static/static-view.styles.js
@@ -25,6 +25,7 @@ export default css`
 
   .static-page-container {
     display: block;
+    height: 100%;
     background-image: url('src/assets/img/background_welcome.jpg');
     background-size: cover;
     background-attachment: fixed;
