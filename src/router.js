066c96e172c89749643202d2686bfd75ca350869
Lars Wirnhier <info@wirnhier.de>
2020-06-14 13:16:20 +0200

    content-files

--- src/router.js
+++ src/router.js
@@ -33,7 +33,7 @@ const TABS = {
   VISUAL: 'visual',
   SEARCH: 'search',
 };
-/*
+
 const TABS_IN_ORDER = [
   TABS.HOME,
   TABS.PALI,
@@ -42,7 +42,7 @@ const TABS_IN_ORDER = [
   TABS.CHINESE,
   TABS.VISUAL
 ];
-*/
+
 const ROUTES = [
   {
     path: '/',
@@ -257,7 +257,7 @@ class BNRouter {
     await this.router.setRoutes(ROUTES);
   }
 
-  ///static selectTab(tabName) {
+  static selectTab(tabName) {
   // this is commented out because we need to change the menu bar first
   // const vaadinTabs = getMainLayout().querySelectorAll('vaadin-tab');
   // vaadinTabs.forEach(item => {
@@ -265,7 +265,7 @@ class BNRouter {
   // });
 
   // vaadinTabs[TABS_IN_ORDER.indexOf(tabName)].setAttribute('selected', 'true');
-  ///}
+  }
 }
 
 export default BNRouter;
