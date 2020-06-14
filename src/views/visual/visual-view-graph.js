d4d2ae014331ac285b239868d0be1907f9f701a8
Hubert Dworczynski <hubert.dworczynski@gmail.com>
2020-06-15 00:12:20 +0200

    Update visual-view-graph.js

--- src/views/visual/visual-view-graph.js
+++ src/views/visual/visual-view-graph.js
@@ -54,12 +54,12 @@ export class VisualViewGraph extends LitElement {
     // smaller collections are enlarged, resulting in a compression effect that makes
     // the rendering of smaller entities more readable. a value of ** 1 means nothing
     // is changed. The smaller the value is, the stronger the graph is 'compressed'.
-      value = value ** 0.2;
-      // we force this min. value of 2 for each connection in order to avoid elements in the graph that are too tiny to be visible.
-      if(value < 2){
-	  value = 2;
-      }
-      return value;
+    value = value ** 0.2;
+    // we force this min. value of 2 for each connection in order to avoid elements in the graph that are too tiny to be visible.
+    if (value < 2) {
+      value = 2;
+    }
+    return value;
   }
 
   changeColorScheme() {
