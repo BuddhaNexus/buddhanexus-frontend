90d43c12f725e252e7c22f32a64ed4217887384a
Hubert Dworczynski <hubert.dworczynski@gmail.com>
2020-06-16 00:41:14 +0200

    [63] improve text-view vertical sizing

--- src/views/textview/text-view-table.styles.js
+++ src/views/textview/text-view-table.styles.js
@@ -3,7 +3,7 @@ import { css } from 'lit-element';
 export default css`
   vaadin-split-layout {
     --_material-split-layout-splitter-background-color: #6d5f47;
-    height: calc(100vh - 460px);
+    height: calc(100vh - 410px);
   }
 
   vaadin-split-layout div {
