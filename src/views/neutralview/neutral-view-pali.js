b147d210d35b5f0abb9562754078a98ba182fca2
Lars Wirnhier <info@wirnhier.de>
2020-06-20 13:05:16 +0200

    text box on language static site

--- src/views/neutralview/neutral-view-pali.js
+++ src/views/neutralview/neutral-view-pali.js
@@ -11,21 +11,27 @@ export class NeutralViewPali extends LitElement {
   }
   render() {
     return html`
-      <h2>Pāli</h2>
-      <p>
-        The Pāli textual corpus used in BuddhaNexus was obtained from the
-        Mahāsaṅgīti Tipiṭaka Buddhavasse 2500: World Tipiṭaka Edition in Roman
-        Script. Edited and published by The M.L. Maniratana Bunnag Dhamma
-        Society Fund, 2005. Based on the digital edition of the Chaṭṭha
-        Saṅgāyana published by the Vipassana Research Institute (VRI), with
-        corrections and proofreading by the Dhamma Society.
-      </p>
-      <p>
-        The texts of the Suttas, Vinaya and Abhidhamma use the SuttaCentral
-        versions as its base and have been adapted for BuddhaNexus. The texts
-        use the same segment numbers as are used in SuttaCentral and link to the
-        corresponding segments on that site.
-      </p>
+      <div class="static-page-container">
+        <div class="main-border">
+          <div class="main-content">
+            <h2>Pāli</h2>
+            <p>
+              The Pāli textual corpus used in BuddhaNexus was obtained from the
+              Mahāsaṅgīti Tipiṭaka Buddhavasse 2500: World Tipiṭaka Edition in
+              Roman Script. Edited and published by The M.L. Maniratana Bunnag
+              Dhamma Society Fund, 2005. Based on the digital edition of the
+              Chaṭṭha Saṅgāyana published by the Vipassana Research Institute
+              (VRI), with corrections and proofreading by the Dhamma Society.
+            </p>
+            <p>
+              The texts of the Suttas, Vinaya and Abhidhamma use the
+              SuttaCentral versions as its base and have been adapted for
+              BuddhaNexus. The texts use the same segment numbers as are used in
+              SuttaCentral and link to the corresponding segments on that site.
+            </p>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
--- src/views/neutralview/neutral-view.styles.js
+++ src/views/neutralview/neutral-view.styles.js
@@ -1,3 +1,38 @@
 import { css } from 'lit-element';
 
-export default css``;
+export default css`
+  .static-page-container {
+    display: block;
+    height: 100%;
+    background-image: url('src/assets/img/background_welcome.jpg');
+    background-size: cover;
+    background-attachment: fixed;
+  }
+
+  .main-border {
+    margin: 0 25%;
+    padding-top: 70px;
+    padding-bottom: 50px;
+  }
+
+  .main-content {
+    background-color: #fff;
+    padding: 14px 40px;
+    border-top: 8px solid #221f19;
+  }
+
+  .main-content h1 {
+    font-size: 1.6em;
+    font-weight: bold;
+  }
+
+  .main-content h2 {
+    font-size: 1.4em;
+    font-weight: bold;
+  }
+
+  .main-content h3 {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+`;
