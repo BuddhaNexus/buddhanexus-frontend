f1627be9dd2e8faa6ab388561a9d60d1a68032e3
ayya-vimala <ayya.vimala@gmail.com>
2020-07-05 13:47:58 +0200

    176 remove bug from visual static page

--- src/views/visual/visual-view.js
+++ src/views/visual/visual-view.js
@@ -58,25 +58,26 @@ export class VisualView extends LitElement {
   render() {
     return html`
       ${this.activeLanguage
-        ? null
+        ? html`
+            <div class="visual-view-container">
+              <visual-view-header
+                .setSelection="${this.setSelection}"
+                .setColorScheme="${this.setColorScheme}"
+                .activeLanguage="${this.activeLanguage}"
+              ></visual-view-header>
+
+              <visual-view-graph
+                .searchItem="${this.searchItem}"
+                .colorScheme="${this.colorScheme}"
+                .selectedCollections="${this.selectedCollections}"
+                .setSelection="${this.setSelection}"
+              >
+              </visual-view-graph>
+            </div>
+          `
         : html`
             <visual-view-selection-box></visual-view-selection-box>
           `}
-      <div class="visual-view-container">
-        <visual-view-header
-          .setSelection="${this.setSelection}"
-          .setColorScheme="${this.setColorScheme}"
-          .activeLanguage="${this.activeLanguage}"
-        ></visual-view-header>
-
-        <visual-view-graph
-          .searchItem="${this.searchItem}"
-          .colorScheme="${this.colorScheme}"
-          .selectedCollections="${this.selectedCollections}"
-          .setSelection="${this.setSelection}"
-        >
-        </visual-view-graph>
-      </div>
     `;
   }
 }
