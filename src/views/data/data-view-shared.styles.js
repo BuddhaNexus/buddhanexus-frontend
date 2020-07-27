80447e6aa428a082c697afbcea457c4dec441a72
ayya-vimala <ayya.vimala@gmail.com>
2020-07-27 12:50:33 +0200

    221 styling global search results

--- src/views/data/data-view-shared.styles.js
+++ src/views/data/data-view-shared.styles.js
@@ -90,6 +90,7 @@ export default css`
     box-shadow: var(--material-card-shadow-inset);
     border: 1px solid #e0e0e0;
     border-radius: 4px;
+    overflow-wrap: break-word;
   }
 
   .horizontal-divider {
