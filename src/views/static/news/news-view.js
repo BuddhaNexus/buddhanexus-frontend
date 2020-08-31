87a3285689d83ff1e0fb9f312da907bb1d60cc20
Sebastian <basti@slayer>
2020-08-31 12:55:47 +0200

    Added some news anouncements.

--- src/views/static/news/news-view.js
+++ src/views/static/news/news-view.js
@@ -13,7 +13,22 @@ export class NewsView extends LitElement {
       <div class="static-page-container">
         <div class="main-border">
           <div class="main-content">
-            <p class="construction">News PAGE</p>
+<h2>August 31, 2020: Addition of the GRETIL Sanskrit data</h2>
+We are happy to announce that now the Sanskrit data from the GRETIL collection is available on the BuddhaNexus: <a href="https://buddhanexus.net/skt/neutral">https://buddhanexus.net/skt/neutral</a></p>
+<h2>August 21, 2020: Online publication of BuddhaNexus</h2>
+<p>We are happy to announce the online publication of the BuddhaNexus database at 
+<a href="https://buddhanexus.net/">https://buddhanexus.net/</a>.</p>
+<p>
+BuddhaNexus, a collaborative project of the Khyentse Center, Universität Hamburg,
+ is a database devoted to the study of Buddhist texts and literary corpora in Pāli, Sanskrit, Tibetan,
+ and Chinese—with particular emphasis on evolution of scriptures, formation of canons,
+ and intellectual networks—by way of locating textual matches within various Buddhist literary corpora and texts.
+ The current version includes material in Pāli, Tibetan, and Chinese. Sanskrit material will be published soon.</p><p>
+Several other features are being currently developed, including linking to other databases and generating translingual matches.</p><p>
+For questions and/or feedback, please contact us under <a href="mailto:buddhanexus.info@gmail.com">buddhanexus.info@gmail.com</a>.</p><p>
+With regards,<br/>
+BuddhaNexus Team
+</p>
           </div>
         </div>
       </div>
