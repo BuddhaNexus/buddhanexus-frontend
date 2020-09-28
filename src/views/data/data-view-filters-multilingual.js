97388c74ec4f26f030db844aa12f50606609cb60
Sebastian <basti@slayer>
2020-09-28 13:40:24 +0200

    Removed unnecessary css.

--- src/views/data/data-view-filters-multilingual.js
+++ src/views/data/data-view-filters-multilingual.js
@@ -2,48 +2,43 @@ import { customElement, html, css, LitElement, property } from 'lit-element';
 
 import '@vaadin/vaadin-checkbox/theme/material/vaadin-checkbox';
 
-import { getMultilingualData } from '../../api/actions';
+import {
+  getMultilingualData,
+
+} from '../../api/actions';
 import { LANGUAGE_CODES } from '../utility/constants';
 
 import styles from './data-view-filters-container.styles';
 
 @customElement('data-view-filters-multilingual')
 export class DataViewFiltersMultilingual extends LitElement {
-  @property({ type: String }) fileName;
-  @property({ type: Array }) multiLangList;
-  @property({ type: Array }) mainLang;
-  @property({ type: Function }) updateMultiLingualMode;
-  @property({ type: Boolean }) dataLoading = true;
-  @property({ type: Boolean }) shouldShowMultiLingual = false;
-  @property({ type: String }) dataLoadError = false;
+    @property({ type: String }) fileName;
+    @property({ type: Array }) multiLangList;
+    @property({ type: Array }) mainLang;
+    @property({ type: Function }) updateMultiLingualMode;
+    @property({ type: Boolean }) dataLoading = true;
+    @property({ type: Boolean }) shouldShowMultiLingual = false;
+    @property({ type: String }) dataLoadError = false;
 
   static get styles() {
     return [
-      styles,
-      css`
-        .loading-spinner-container {
-          width: 100%;
-          height: 100%;
-          margin-top: 5em;
-          position: relative;
-        }
-      `,
-    ];
+	styles]
   }
 
-  firstUpdated() {
+    firstUpdated() {
     this.fetchMultilingualData();
   }
 
-  updated(_changedProperties) {
+    updated(_changedProperties) {
     _changedProperties.forEach(async (oldValue, propName) => {
-      if (propName === 'fileName' && !this.dataLoading) {
-        this.fetchMultilingualData();
-      }
+	if (propName === 'fileName' && !this.dataLoading) {
+	    this.fetchMultilingualData();
+	}
     });
-  }
+    }
 
-  async fetchMultilingualData() {
+    
+    async fetchMultilingualData() {
     this.dataLoading = true;
     const { langList, error } = await getMultilingualData({
       fileName: this.fileName,
@@ -53,96 +48,45 @@ export class DataViewFiltersMultilingual extends LitElement {
     this.dataLoading = false;
   }
 
-  renderPali() {
-    if (this.mainLang == LANGUAGE_CODES.PALI) {
-      return html`
-        <vaadin-checkbox
-          value="pli"
-          @checked-changed="${this.updateMultiLingualMode}"
-          checked
-          >Pāḷi</vaadin-checkbox
-        >
-      `;
-    } else if (this.multiLangList.includes(LANGUAGE_CODES.PALI)) {
-      return html`
-        <vaadin-checkbox
-          value="pli"
-          @checked-changed="${this.updateMultiLingualMode}"
-          unchecked
-          >Pāḷi</vaadin-checkbox
-        >
-      `;
-    }
-  }
+    renderPali(){
+	if(this.mainLang == LANGUAGE_CODES.PALI){
+	    return html`<vaadin-checkbox value="pli" @checked-changed="${this.updateMultiLingualMode}" checked>Pāḷi</vaadin-checkbox>`
+	}	
+	else if(this.multiLangList.includes(LANGUAGE_CODES.PALI)){
+	    return html`<vaadin-checkbox value="pli" @checked-changed="${this.updateMultiLingualMode}" unchecked>Pāḷi</vaadin-checkbox>`
+	};
+	       }
 
-  renderSanskrit() {
-    if (this.mainLang == LANGUAGE_CODES.SANSKRIT) {
-      return html`
-        <vaadin-checkbox
-          value="skt"
-          @checked-changed="${this.updateMultiLingualMode}"
-          checked
-          >Sanskrit</vaadin-checkbox
-        >
-      `;
-    } else if (this.multiLangList.includes(LANGUAGE_CODES.SANSKRIT)) {
-      return html`
-        <vaadin-checkbox
-          value="skt"
-          @checked-changed="${this.updateMultiLingualMode}"
-          unchecked
-          >Sanskrit</vaadin-checkbox
-        >
-      `;
-    }
-  }
+    renderSanskrit(){
+	if(this.mainLang == LANGUAGE_CODES.SANSKRIT){	    
+	    return html`<vaadin-checkbox value="skt" @checked-changed="${this.updateMultiLingualMode}" checked>Sanskrit</vaadin-checkbox>`
+	}	
+	else if(this.multiLangList.includes(LANGUAGE_CODES.SANSKRIT)){
+	    return html`<vaadin-checkbox value="skt" @checked-changed="${this.updateMultiLingualMode}" unchecked>Sanskrit</vaadin-checkbox>`
+	};
+	       }
 
-  renderTibetan() {
-    if (this.mainLang == LANGUAGE_CODES.TIBETAN) {
-      return html`
-        <vaadin-checkbox
-          value="tib"
-          @checked-changed="${this.updateMultiLingualMode}"
-          checked
-          >Tibetan</vaadin-checkbox
-        >
-      `;
-    } else if (this.multiLangList.includes(LANGUAGE_CODES.TIBETAN)) {
-      return html`
-        <vaadin-checkbox
-          value="tib"
-          @checked-changed="${this.updateMultiLingualMode}"
-          unchecked
-          >Tibetan</vaadin-checkbox
-        >
-      `;
-    }
-  }
-  renderChinese() {
-    if (this.mainLang == LANGUAGE_CODES.CHINESE) {
-      return html`
-        <vaadin-checkbox
-          value="chn"
-          @checked-changed="${this.updateMultiLingualMode}"
-          checked
-          >Chinese</vaadin-checkbox
-        >
-      `;
-    } else if (this.multiLangList.includes(LANGUAGE_CODES.CHINESE)) {
-      return html`
-        <vaadin-checkbox
-          value="chn"
-          @checked-changed="${this.updateMultiLingualMode}"
-          unchecked
-          >Chinese</vaadin-checkbox
-        >
-      `;
-    }
-  }
+    renderTibetan(){	
+	if(this.mainLang == LANGUAGE_CODES.TIBETAN){
+	    return html`<vaadin-checkbox value="tib" @checked-changed="${this.updateMultiLingualMode}" checked>Tibetan</vaadin-checkbox>`
+	}	
+	else if(this.multiLangList.includes(LANGUAGE_CODES.TIBETAN)){
+	    return html`<vaadin-checkbox value="tib" @checked-changed="${this.updateMultiLingualMode}" unchecked>Tibetan</vaadin-checkbox>`
+	};
+	       }
+    renderChinese(){
+	if(this.mainLang == LANGUAGE_CODES.CHINESE){
+	    return html`<vaadin-checkbox value="chn" @checked-changed="${this.updateMultiLingualMode}" checked>Chinese</vaadin-checkbox>`
+	}	
+	else if(this.multiLangList.includes(LANGUAGE_CODES.CHINESE)){
+	    return html`<vaadin-checkbox value="chn" @checked-changed="${this.updateMultiLingualMode}" unchecked>Chinese</vaadin-checkbox>`
+	};
+	       }
 
+	
   render() {
-    //prettier-ignore
-    if(!this.dataLoading && this.shouldShowMultiLingual){
+      //prettier-ignore
+      if(!this.dataLoading && this.shouldShowMultiLingual()){
     return html`
       <div id="multi-lingual-label">Choose Match Languages:</div>
       <div>
