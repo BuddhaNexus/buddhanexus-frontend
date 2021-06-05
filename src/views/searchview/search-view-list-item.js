f6d953b7f165254cb37f8de72a4e0485bf206215
ayya-vimala <ayya.vimala@gmail.com>
2021-06-05 13:29:55 +0200

    add message to multilingual search results

--- src/views/searchview/search-view-list-item.js
+++ src/views/searchview/search-view-list-item.js
@@ -8,7 +8,12 @@ export default function SearchViewListItem({
   SegmentText,
   distance,
   rootUrl,
+  multiResults,
 }) {
+  let multiLangAvailable = false;
+  if (multiResults.length > 0) {
+    multiLangAvailable = true;
+  }
   //prettier-ignore
   return html`
     <div class="search-view-list__item">
@@ -19,7 +24,10 @@ export default function SearchViewListItem({
               .segmentnr="${[SegmentId,]}"
               .lang="${getLanguageFromFilename(SegmentId)}">
             </formatted-segment>
-          </span><span>Edit distance: ${distance}</span>
+          </span><span>Edit distance: ${distance} </span> 
+          <span class="trans-message" style="display: ${
+              multiLangAvailable ? 'block' : 'none'
+            }"> multiligngual data vailable</span>
           <div class="search-view-list__parallel-details">
             <span class="search-view-list__parallel-details-badge">
           </div>
