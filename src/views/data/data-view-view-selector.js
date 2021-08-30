6a3ffe1f50f7f49e961522a91fa71ab85839e2f8
ayya-vimala <ayya.vimala@gmail.com>
2021-08-30 14:52:22 +0200

    409 alternative for just pli and chn

--- src/views/data/data-view-view-selector.js
+++ src/views/data/data-view-view-selector.js
@@ -16,56 +16,75 @@ export class DataViewViewSelector extends LitElement {
         .visibility-filters {
           margin-left: 0;
           margin-right: 2em;
-          width: 100px;
         }
 
         vaadin-select {
           --material-primary-color: var(--bn-dark-red);
           --material-primary-text-color: var(--bn-dark-red);
+          width: 100px;
         }
       `,
     ];
   }
 
   render() {
+    const shouldShowRadioButtons =
+      this.language !== 'pli' && this.language !== 'chn';
     return html`
-      <vaadin-select
-        value="${this.viewMode}"
-        label="Choose view:"
-        class="visibility-filters"
-        @value-changed="${e => this.handleViewModeChanged(e.target.value)}"
-      >
-        <template>
-          <vaadin-list-box>
-            ${Object.values(DATA_VIEW_MODES).map(filter => {
-              if (
-                (filter !== 'numbers' ||
-                  (this.language !== 'tib' &&
-                    this.language !== 'skt' &&
-                    this.language !== 'multi')) &&
-                filter !== 'neutral' &&
-                (filter !== 'multiling' ||
-                  (this.language !== 'skt' &&
-                    this.language !== 'tib' &&
-                    this.language !== 'chn' &&
-                    this.language !== 'pli')) &&
-                filter !== 'text-search' &&
-                (filter !== 'english' ||
-                  (this.language !== 'skt' &&
-                    this.language !== 'tib' &&
-                    this.language !== 'chn' &&
-                    this.language !== 'multi'))
-              ) {
-                return html`
-                  <vaadin-item style="text-transform: capitalize"
-                    >${filter}</vaadin-item
-                  >
-                `;
-              }
-            })}
-          </vaadin-list-box>
-        </template>
-      </vaadin-select>
+      ${shouldShowRadioButtons
+        ? html`
+            <vaadin-radio-group
+              label="Choose view:"
+              class="visibility-filters"
+              value="${this.viewMode}"
+              @value-changed="${e =>
+                this.handleViewModeChanged(e.target.value)}"
+            >
+              ${Object.values(DATA_VIEW_MODES).map(filter => {
+                if (
+                  filter !== 'numbers' &&
+                  filter !== 'neutral' &&
+                  (filter !== 'multiling' || this.language === 'multi') &&
+                  filter !== 'text-search' &&
+                  filter !== 'english'
+                ) {
+                  return html`
+                    <vaadin-radio-button value="${filter}">
+                      ${filter}
+                    </vaadin-radio-button>
+                  `;
+                }
+              })}
+            </vaadin-radio-group>
+          `
+        : html`
+            <vaadin-select
+              value="${this.viewMode}"
+              label="Choose view:"
+              class="visibility-filters"
+              @value-changed="${e =>
+                this.handleViewModeChanged(e.target.value)}"
+            >
+              <template>
+                <vaadin-list-box>
+                  ${Object.values(DATA_VIEW_MODES).map(filter => {
+                    if (
+                      filter !== 'neutral' &&
+                      filter !== 'multiling' &&
+                      filter !== 'text-search' &&
+                      (filter !== 'english' || this.language === 'pli')
+                    ) {
+                      return html`
+                        <vaadin-item style="text-transform: capitalize"
+                          >${filter}</vaadin-item
+                        >
+                      `;
+                    }
+                  })}
+                </vaadin-list-box>
+              </template>
+            </vaadin-select>
+          `}
     `;
   }
 }
